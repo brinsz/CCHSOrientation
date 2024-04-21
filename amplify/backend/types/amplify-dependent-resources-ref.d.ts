b32b96ccefbc52815f02c2b3417249c6cd0e9b7e
Matthew <matthha@umich.edu>
2024-04-21 09:46:08 -0400

    Update amp

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "CCHSOrientation": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "CCHSOrientation": {
       "AppClientID": "string",
